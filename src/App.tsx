4c58247216028bcbe3daaf3f10b44da4fa245540
muhamsh <muhammad-ashraful@outlook.com>
2025-08-06 20:31:57 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+      <Button className='text-teal-100'>Button</Button>
+    </div>
+  )
+}
+
+export default App
