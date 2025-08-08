508977b0364a5dde63560c2c8afac3e36ee5cc52
muhamsh <muhammad-ashraful@outlook.com>
2025-08-09 02:55:12 +0600

    redux toolkit init

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router'
-import App from './App.tsx'
-import './index.css'
-import { appRouter } from './routes/index.ts'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { RouterProvider } from 'react-router';
+import App from './App.tsx';
+import './index.css';
+import { store } from './redux/store';
+import { appRouter } from './routes/index.ts';
 
-createRoot(document.getElementById('root')!).render(
+createRoot( document.getElementById( 'root' )! ).render(
   <StrictMode>
-    <RouterProvider router={appRouter}>
-      <App />
-    </RouterProvider>
+    <Provider store={store}>
+      <RouterProvider router={appRouter}>
+        <App />
+      </RouterProvider>
+    </Provider>
   </StrictMode>,
-)
+);
