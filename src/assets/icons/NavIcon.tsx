bdf924e05c9d15664f9b6e04bb446f482e56495a
muhamsh <muhammad-ashraful@outlook.com>
2025-08-08 22:14:02 +0600

    nav added

---
+++ src/assets/icons/NavIcon.tsx
@@ -0,0 +1,9 @@
+
+export default function NavIcon() {
+    return (
+        <svg width="50" height="50" viewBox="0 0 30 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15 0L20.4545 5.33333L0 25.3333V14.6667L15 0Z" fill="#0f8447"></path>
+            <path d="M2.90827 28.177L15 40L30 25.3334V14.6667L20.4545 5.33337L0 25.3334L0.0041688 25.3375L20.4545 5.33337V20.6667L11.25 29.6667V20.1324L2.90827 28.177Z" fill="#668695"></path>
+        </svg>
+    );
+}
