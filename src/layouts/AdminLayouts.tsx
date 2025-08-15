94ecd9129e6ba95c889eca9fd517b45896fd9fca
muhamsh <muhammad-ashraful@outlook.com>
2025-08-15 20:39:46 +0600

    integrating sidebar

--- src/layouts/AdminLayouts.tsx
+++ src/layouts/AdminLayouts.tsx
@@ -1,17 +1,48 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import { Separator } from "@/components/ui/separator";
+import
+    {
+        SidebarInset,
+        SidebarProvider,
+        SidebarTrigger,
+    } from "@/components/ui/sidebar";
 import { Outlet } from "react-router";
 
-// interface IAdminLayout
-// {
-//     children: ReactNode;
-// }
-
-const AdminLayouts = () => {
+export default function DashboardLayout ()
+{
+    
     return (
-        <div>
-            <h1>Admin layout!!</h1>
-            <Outlet/>
-        </div>
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator
+                        orientation="vertical"
+                        className="mr-2 data-[orientation=vertical]:h-4"
+                    />
+
+                    {/* no need
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem className="hidden md:block">
+                                <BreadcrumbLink href="#">
+                                    Building Your Application
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator className="hidden md:block" />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb> */}
+
+                </header>
+                <div className="flex flex-1 flex-col gap-4 p-4">
+                    <Outlet/>
+                </div>
+
+            </SidebarInset>
+        </SidebarProvider>
     );
 };
-
-export default AdminLayouts;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
+import AdminLayouts from "../layouts/AdminLayouts";
 import ABoutPage from "../pages/about/Page";
+import AnalyticsPage from "../pages/analytics/page";
 import LoginPage from "../pages/login/page";
 import NotFoundPage from "../pages/notFound/page";
 import RegisterPage from "../pages/register/page";
@@ -14,6 +16,16 @@ export const appRouter = createBrowserRouter( [
             {
                 Component: ABoutPage,
                 path: "/about"
+            }
+        ]
+    },
+    {
+        Component: AdminLayouts,
+        path: "/admin",
+        children: [
+            {
+                Component: AnalyticsPage,
+                path: '/admin/analytics'
             }
         ]
     },
