6d9fc1eb80729527a18d7fe09b034efd2642f6d5
muhamsh <muhammad-ashraful@outlook.com>
2025-08-16 23:30:56 +0600

    tour base query

--- src/pages/tour/addTourType/page.tsx
+++ src/pages/tour/addTourType/page.tsx
@@ -1,5 +1,10 @@
+import { useGetTourTypesQuery } from "../../../redux/features/api/tour/tour.api";
 
-export default function AddTourType() {
+export default function AddTourType ()
+{
+  const { data } = useGetTourTypesQuery();
+  console.log( data );
+  
   return (
     <div className="text-muted-foreground">
       add Tour type
